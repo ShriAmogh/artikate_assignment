c49ede8ec3e00bc816ece3b755c33b96ab1fe5a6
Amogh Arora <amogh.arora@hotmail.com>
2026-01-14 23:28:21 +0530

    miscellaneous

--- main_ingestor.py
+++ main_ingestor.py
@@ -1,12 +1,13 @@
-from rag.data_ingestor.ingestion import FolderPDFIngestor
+from rag_django.rag.data_ingestor.ingestion import FolderPDFIngestor
 from config import chunk_size, overlap_size
 
+#for testing rag system before integratiing with django. 
 ingestor = FolderPDFIngestor(
     folder_path="G:/Artikate_assignment/rag/data/",
     chroma_dir="vector_store/",
     collection_name="science_class_9",
-    chunk_size= chunk_size,
-    overlap_size= overlap_size
+    chunk_size= 500,
+    chunk_overlap= 350
 )
 
 result = ingestor.ingest()
